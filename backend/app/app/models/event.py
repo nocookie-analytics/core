1165b87945babbe6b42a38bba0226c545478b289
Gaganpreet <me@gaganpreet.in>
2020-05-15 23:54:41 +0200

    Add some fields

--- backend/app/app/models/event.py
+++ backend/app/app/models/event.py
@@ -1,7 +1,14 @@
+from enum import Enum
 from sqlalchemy import Column, ForeignKey, Integer, String, TIMESTAMP
 from sqlalchemy.orm import relationship
+from sqlalchemy_enum34 import EnumType
+from sqlalchemy.dialects.postgresql import JSONB
 
 from app.db.base_class import Base
+
+
+class EventType(Enum):
+    page_view = 'page_view'
 
 
 class Event(Base):
@@ -10,3 +17,16 @@ class Event(Base):
 
     visitor_id = Column(Integer, ForeignKey("visitor.id"))
     visitor = relationship("Visitor", back_populates="events")
+
+    event_type = Column(EnumType(EventType), nullable=False)
+
+    ua_string = Column(String)
+    path = Column(String)
+    url_params = Column(JSONB)
+
+    page_title = Column(String)
+    status_code = Column(Integer)
+    load_time_ms = Column(Integer)
+    page_size_bytes = Column(Integer)
+    referrer = Column(String)
+    user_timezone = Column(String)
