a06807ac5dbc9e956042d99ee06526e9ee54be95
Gaganpreet Arora <me@gaganpreet.in>
2020-09-02 23:09:55 +0200

    add crud event module

---
+++ backend/app/app/crud/crud_event.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from app.models.event import Event
+from app.schemas.event import EventCreate
+
+
+class CRUDEvent(CRUDBase[Event, EventCreate, EventUpdate]):
+    def create_with_domain(
+        self, db: Session, *, obj_in: EventCreate, domain_id: int
+    ) -> Event:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data, domain_id=domain_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+
+event = CRUDEvent(Event)
+
