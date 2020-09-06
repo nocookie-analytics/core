50ecec767a4db2ca8e7e6cfc034c5cbce542f10a
Gaganpreet Arora <me@gaganpreet.in>
2020-09-06 20:52:43 +0200

    add dummy event api

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -2,4 +2,4 @@ from .domain import Domain, DomainCreate, DomainInDB, DomainUpdate
 from .msg import Msg
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
-from .event import EventCreate
+from .event import EventCreate, EventCreated
