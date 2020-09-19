a1b7a141b51c5b0ab8fcbe5391c1b4512db7ab36
Gaganpreet Arora <me@gaganpreet.in>
2020-09-19 22:50:22 +0200

    load page time

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,9 +1,40 @@
-import Fingerprint2, {Component} from 'fingerprintjs2';
-import objectHash from 'object-hash';
+import Fingerprint2, { Component } from "fingerprintjs2";
+import objectHash from "object-hash";
+import Perfume from "perfume.js";
+import { IPerfumeNavigationTiming } from "perfume.js/dist/types/types";
 
 const getComponents = () => {
-    Fingerprint2.getPromise().then(handleComponents);
+  Fingerprint2.getPromise().then(handleComponents);
 };
+
+const perfume = new Perfume({
+  resourceTiming: true,
+  analyticsTracker: ({ metricName, data }) => {
+    data = data as IPerfumeNavigationTiming;
+    if (metricName == "navigationTiming") {
+      const performance = window.performance?.getEntriesByType(
+        "navigation"
+      )[0] as any;
+      const { encodedBodySize } = performance;
+      const {
+        dnsLookupTime,
+        downloadTime,
+        fetchTime,
+        timeToFirstByte,
+        totalTime,
+      } = data;
+
+      console.log(
+        encodedBodySize,
+        dnsLookupTime,
+        downloadTime,
+        fetchTime,
+        timeToFirstByte,
+        totalTime
+      );
+    }
+  },
+});
 
 const handleComponents = (components: Component[]) => {
   const userIdentifier: string = objectHash(components);
@@ -11,7 +42,7 @@ const handleComponents = (components: Component[]) => {
 };
 
 if (window.requestIdleCallback) {
-    requestIdleCallback(getComponents);
+  window.requestIdleCallback(getComponents);
 } else {
-    setTimeout(getComponents, 500);
+  setTimeout(getComponents, 500);
 }
