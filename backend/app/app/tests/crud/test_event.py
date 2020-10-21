25ee5186b978b702ea86b6a05646141eb0bec556
Gaganpreet Arora <me@gaganpreet.in>
2020-10-21 15:30:50 +0200

    remove extra fields

--- backend/app/app/tests/crud/test_event.py
+++ backend/app/app/tests/crud/test_event.py
@@ -13,16 +13,14 @@ def test_create_event(db: Session) -> None:
         path="/abc",
         url_params={},
         page_title="Title",
-        status_code=404,
         page_size_bytes=150,
         referrer="abc",
         user_timezone="Europe/Amsterdam",
         ua_string="Mozilla/5.0 (X11; Linux x86_64; rv:9000.0) Gecko/20100101 Firefox/9000.0",
-        dnslookup_time=5000,
         download_time=5000,
-        fetch_time=5000,
         time_to_first_byte=5000,
         total_time=5000,
+        ip_address="127.0.0.1",
     )
     event = crud.event.create_with_domain(db=db, obj_in=event_in, domain_id=domain.id)
     assert event.domain_id == domain.id
