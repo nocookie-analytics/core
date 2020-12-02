ef1f8b429fd459a6d1aaaaa288da99bc82a4083b
Gaganpreet Arora <me@gaganpreet.in>
2020-12-02 11:24:12 +0100

    update import

--- backend/app/app/api/api_v1/endpoints/analytics.py
+++ backend/app/app/api/api_v1/endpoints/analytics.py
@@ -1,25 +1,26 @@
 from __future__ import annotations
-from app import crud
-import arrow
-from datetime import datetime
-from typing import List, Optional, Tuple
 
+from datetime import datetime
+from typing import List, Optional
+
+import arrow
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from app import AnalyticsType, schemas
+from app import crud
 from app.api import deps
+from app.schemas.analytics import AnalyticsType
 
 router = APIRouter()
 
 
-def parse_end_date(end: Optional[datetime] = None):
+def get_end_date(end: Optional[datetime] = None):
     end_obj = arrow.get(end) if end else arrow.now()
     return end_obj
 
 
-def parse_start_date(
-    end: arrow.Arrow = Depends(parse_end_date), start: Optional[datetime] = None
+def get_start_date(
+    end: arrow.Arrow = Depends(get_end_date), start: Optional[datetime] = None
 ):
     start_obj = arrow.get(start) if start else end.shift(months=-1)
     return start_obj
@@ -28,8 +29,8 @@ def parse_start_date(
 @router.get("/", response_model=List[AnalyticsType])
 def get_analytics(
     db: Session = Depends(deps.get_db),
-    start: datetime = Depends(parse_start_date),
-    end: datetime = Depends(parse_end_date),
+    start: datetime = Depends(get_start_date),
+    end: datetime = Depends(get_end_date),
     include: List[AnalyticsType] = Depends(AnalyticsType.from_csv_string),
 ):
     if start >= end:
