445dcd296fb49c61d86c77557b4192707641b9b1
Gaganpreet Arora <me@gaganpreet.in>
2020-12-15 21:22:19 +0100

    Add index on locations

--- backend/app/app/models/event.py
+++ backend/app/app/models/event.py
@@ -102,11 +102,20 @@ class Event(Base):
     ix_domain_timestamp = Index("ix_domain_timestamp", domain_id, timestamp)
     ix_timestamp = Index("ix_timestamp", timestamp)
     ix_browser_family = Index("ix_browser_family", domain_id, browser_family, timestamp)
-    ix_os_family = Index("ix_os_family", domain_id, os_family, timestamp)
+    ix_city_id = Index("ix_city", domain_id, ip_city_id)
+    ix_country_id = Index("ix_country", domain_id, ip_country_iso_code)
+    ix_os_family = Index(
+        "ix_os_family",
+        domain_id,
+        os_family,
+        os_version_major,
+        os_version_minor,
+        timestamp,
+    )
     ix_device_family = Index("ix_device_family", domain_id, device_family, timestamp)
     ix_device_brand = Index("ix_device_brand", domain_id, device_brand, timestamp)
     ix_device_model = Index("ix_device_model", domain_id, device_model, timestamp)
-    ix_device_metric = Index("ix_device_metric", domain_id, metric_name, timestamp)
+    ix_metric = Index("ix_metric", domain_id, metric_name, timestamp)
 
     # Choosing this as a primary key so the table is partitioned by domain first,
     # then timestamp but the combination of domain and timestamp won't be unique,
