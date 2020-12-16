18c07139e5ca5b15405b8840264306772f8ee489
Gaganpreet Arora <me@gaganpreet.in>
2020-12-16 21:43:18 +0100

    add comment about data source

--- backend/app/data/update-referers.sh
+++ backend/app/data/update-referers.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -euo pipefail
 
+# The data source of this script is here:
+# https://github.com/snowplow-referer-parser/referer-parser
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 curl -s https://s3-eu-west-1.amazonaws.com/snowplow-hosted-assets/third-party/referer-parser/referers-latest.json -o referers.json
