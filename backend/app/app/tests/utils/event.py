0185c1c637df28e75f90a5315a9c9ab142401f3f
Gaganpreet Arora <me@gaganpreet.in>
2020-12-19 22:23:45 +0100

    add referrer api and tests

--- backend/app/app/tests/utils/event.py
+++ backend/app/app/tests/utils/event.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import Optional
+from typing import Dict, Optional
 
 from sqlalchemy.orm import Session
 
@@ -13,21 +13,26 @@ def create_random_page_view_event(
     *,
     domain_id: int,
     ip_address: Optional[str] = None,
+    create_overrides: Dict = None,
 ) -> models.Event:
-    event_in = EventCreate(
-        event_type=EventType.page_view,
-        url="https://google.com",
-        page_title="Title",
-        page_size_bytes=150,
-        referrer="abc",
-        user_timezone="Europe/Amsterdam",
-        ua_string="Mozilla/5.0 (X11; Linux x86_64; rv:9000.0) Gecko/20100101 Firefox/9000.0",
-        download_time=5000,
-        time_to_first_byte=5000,
-        total_time=5000,
-        ip_address=ip_address or "127.0.0.1",
-        page_view_id=uuid.uuid4(),
-    )
+    if not create_overrides:
+        create_overrides = {}
+    params = {
+        "event_type": EventType.page_view,
+        "url": "https://google.com",
+        "page_title": "Title",
+        "page_size_bytes": 150,
+        "referrer": "abc",
+        "user_timezone": "Europe/Amsterdam",
+        "ua_string": "Mozilla/5.0 (X11; Linux x86_64; rv:9000.0) Gecko/20100101 Firefox/9000.0",
+        "download_time": 5000,
+        "time_to_first_byte": 5000,
+        "total_time": 5000,
+        "ip_address": ip_address or "127.0.0.1",
+        "page_view_id": uuid.uuid4(),
+        **create_overrides,
+    }
+    event_in = EventCreate(**params)
     return crud.event.create_with_domain(db=db, obj_in=event_in, domain_id=domain_id)
 
 
