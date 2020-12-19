f8d4531f3d1034a5e4be437e51dc12e6cfcf99e9
Gaganpreet Arora <me@gaganpreet.in>
2020-12-19 22:54:36 +0100

    fix celery 5.0 commands

--- backend/app/worker-start.sh
+++ backend/app/worker-start.sh
@@ -3,4 +3,4 @@ set -e
 
 python /app/app/celeryworker_pre_start.py
 
-C_FORCE_ROOT=1 celery worker -A app.worker -l info -Q main-queue -c 1
+C_FORCE_ROOT=1 celery -A app.worker worker -l info -Q main-queue -c 1
