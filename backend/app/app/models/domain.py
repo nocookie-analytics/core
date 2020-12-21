a5433752d92b4fb281b05fd1cd944afc0123311b
Gaganpreet Arora <me@gaganpreet.in>
2020-12-21 21:37:04 +0100

    update type annotation

--- backend/app/app/models/domain.py
+++ backend/app/app/models/domain.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, List
 
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Query
 from sqlalchemy.sql.functions import func
 from sqlalchemy.sql.sqltypes import DateTime
 
@@ -10,14 +10,13 @@ from app.db.base_class import Base
 
 if TYPE_CHECKING:
     from .user import User  # noqa: F401
-    from .event import Event
 
 
 class Domain(Base):
     id = Column(Integer, primary_key=True, index=True)
 
     domain_name = Column(String, index=True, unique=True)
-    events: List[Event] = relationship("Event", lazy="dynamic")
+    events: Query = relationship("Event", lazy="dynamic")
 
     owner_id = Column(Integer, ForeignKey("user.id"))
     owner: User = relationship("User", back_populates="domains")
