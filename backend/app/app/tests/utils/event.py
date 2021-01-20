6a22e89c1251eed1e0ce3467631fd9b98dee55db
Gaganpreet Arora <me@gaganpreet.in>
2021-01-20 21:43:17 +0100

    allow create overrides for metric event fixture

--- backend/app/app/tests/utils/event.py
+++ backend/app/app/tests/utils/event.py
@@ -41,21 +41,24 @@ def create_random_metric_event(
     *,
     domain_id: int,
     ip_address: Optional[str] = None,
+    create_overrides: Dict = None,
 ) -> models.Event:
-    event_in = EventCreate(
-        event_type=EventType.metric,
-        url="https://google.com",
-        page_title="Title",
-        page_size_bytes=150,
-        referrer="abc",
-        user_timezone="Europe/Amsterdam",
-        ua_string="Mozilla/5.0 (X11; Linux x86_64; rv:9000.0) Gecko/20100101 Firefox/9000.0",
-        download_time=5000,
-        time_to_first_byte=5000,
-        total_time=5000,
-        ip_address=ip_address or "127.0.0.1",
-        page_view_id=uuid.uuid4(),
-        metric_name=MetricType.LCP,
-        metric_value=1234,
-    )
+    event_in_data = {
+        "event_type": EventType.metric,
+        "url": "https://google.com",
+        "page_title": "Title",
+        "page_size_bytes": 150,
+        "referrer": "abc",
+        "user_timezone": "Europe/Amsterdam",
+        "ua_string": "Mozilla/5.0 (X11; Linux x86_64; rv:9000.0) Gecko/20100101 Firefox/9000.0",
+        "download_time": 5000,
+        "time_to_first_byte": 5000,
+        "total_time": 5000,
+        "ip_address": ip_address or "127.0.0.1",
+        "page_view_id": uuid.uuid4(),
+        "metric_name": MetricType.LCP,
+        "metric_value": 1234,
+        **create_overrides,
+    }
+    event_in = EventCreate(**event_in_data)
     return crud.event.create_with_domain(db=db, obj_in=event_in, domain_id=domain_id)
