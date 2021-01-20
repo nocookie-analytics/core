b4987921a8a801edcb4ce3796992cd894656faab
Gaganpreet Arora <me@gaganpreet.in>
2021-01-20 21:49:08 +0100

    Add test for avg metric class

--- backend/app/app/tests/schemas/test_analytics.py
+++ backend/app/app/tests/schemas/test_analytics.py
@@ -1,9 +1,11 @@
+from datetime import date
 import pytest
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm.session import Session
 
-from app.models.event import Event, EventType
+from app.models.event import Event, EventType, MetricType
 from app.schemas.analytics import (
+    AvgMetricPerDayStat,
     PageViewsPerDayStat,
     AggregateStat,
     AnalyticsType,
@@ -100,6 +102,40 @@ class TestAggregateStat:
         assert data[0].total_visits == 1
 
 
-class AvgMetricPerDayStatTest:
-    def test_avg(self):
-        ...
+class TestAvgMetricPerDayStat:
+    def test_avg(self, db, mock_ip_address):
+        domain = create_random_domain(db)
+        event = create_random_page_view_event(
+            db,
+            domain_id=domain.id,
+            ip_address=mock_ip_address,
+        )
+        create_random_metric_event(
+            db,
+            domain_id=domain.id,
+            create_overrides={
+                "page_view_id": event.page_view_id,
+                "metric_value": 5,
+                "metric_name": MetricType.LCP,
+            },
+        )
+        base_query = domain.events.filter(Event.event_type == EventType.metric)
+        lcp_per_day = AvgMetricPerDayStat.from_base_query(base_query, MetricType.LCP)
+        assert len(lcp_per_day) == 1
+        assert lcp_per_day[0].value == 5
+
+        create_random_metric_event(
+            db,
+            domain_id=domain.id,
+            create_overrides={
+                "page_view_id": event.page_view_id,
+                "metric_value": 10,
+                "metric_name": MetricType.LCP,
+            },
+        )
+
+        lcp_per_day = AvgMetricPerDayStat.from_base_query(base_query, MetricType.LCP)
+        fid_per_day = AvgMetricPerDayStat.from_base_query(base_query, MetricType.FID)
+        assert len(lcp_per_day) == 1
+        assert not fid_per_day
+        assert lcp_per_day[0].value == 7.5
