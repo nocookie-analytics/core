7b0b19e834055aa7780d1b509efcddfe523e527b
Gaganpreet Arora <me@gaganpreet.in>
2021-01-27 19:17:08 +0100

    try some colours

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,10 +1,24 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
+const colors = {
+    newOrleans: '#edd693',
+    flamePea: '#e75f3b',
+};
+
 export default new Vuetify({
-  icons: {
-    iconfont: 'mdi',
-  },
+    icons: {
+        iconfont: 'mdi',
+    },
+    theme: {
+        themes: {
+            light: {
+                secondary: colors.newOrleans,
+                primary: colors.flamePea,
+            },
+        },
+    },
 });
