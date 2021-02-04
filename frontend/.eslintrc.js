ca6e149101d5a3ef8cf305cc3c0b49f183002d54
Gaganpreet Arora <me@gaganpreet.in>
2021-02-04 22:27:47 +0100

    use regenerated config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,45 +1,22 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es6: true,
     node: true,
   },
-
-  parser: '@typescript-eslint/parser',
-
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-    extraFileExtensions: ['.vue'],
-    ecmaVersion: 2020,
-  },
-
-  plugins: [
-    'eslint-plugin-import',
-    'eslint-plugin-jsdoc',
-    'eslint-plugin-prefer-arrow',
-    '@typescript-eslint',
-    '@typescript-eslint/tslint',
-  ],
-
-  root: true,
-
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:vue/essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
   ],
-
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-
   overrides: [
     {
       files: [
@@ -47,7 +24,7 @@ module.exports = {
         '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true,
+        mocha: true,
       },
     },
   ],
