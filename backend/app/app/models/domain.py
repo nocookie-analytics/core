b0b647310c3f5aed297d85a839630a798abbc03d
Gaganpreet Arora <me@gaganpreet.in>
2021-02-08 21:26:57 +0100

    fix model

--- backend/app/app/models/domain.py
+++ backend/app/app/models/domain.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING, List
 
-from sqlalchemy import Column, ForeignKey, Integer, String, public, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, Query
 from sqlalchemy.sql.functions import func
 from sqlalchemy.sql.sqltypes import DateTime
@@ -26,4 +26,4 @@ class Domain(Base):
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
 
-    public = Column(Boolean, default=False, server_default=False)
+    public = Column(Boolean, nullable=False, default=False, server_default='false')
