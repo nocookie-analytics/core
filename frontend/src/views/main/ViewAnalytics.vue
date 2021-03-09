eaad383b07dfa1a655161533f5d656bda2d9ab9d
Gaganpreet Arora <me@gaganpreet.in>
2021-03-09 21:44:32 +0100

    Fetch domain info

--- frontend/src/views/main/ViewAnalytics.vue
+++ frontend/src/views/main/ViewAnalytics.vue
@@ -7,9 +7,14 @@
           <div class="headline primary--text">View Analytics</div>
         </v-card-title>
         <v-card-text>
-          <div class="headline font-weight-light ma-5">{{ domainName }}</div>
+          <div class="headline font-weight-light ma-5">
+            {{ domainName }}
+            <span v-if="domainError"
+              >- {{ domainError }} (You might need to
+              <a href="/login">log-in</a>)</span
+            >
+          </div>
         </v-card-text>
-        <ChoroplethMap></ChoroplethMap>
       </v-card>
     </v-container>
   </div>
@@ -17,10 +22,23 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import ChoroplethMap from '@/components/ChoroplethMap.vue';
+import { Domain, DomainsApi } from '@/generated';
 
 @Component
 export default class ViewAnalytics extends Vue {
+  public domainInfo: Domain | null = null;
+  public domainError = '';
+
+  public async mounted(): Promise<void> {
+    const api: DomainsApi = this.$store.getters.domainsApi;
+    try {
+      const response = await api.readDomainByName(this.domainName);
+      this.domainInfo = response.data;
+    } catch (error) {
+      this.domainError = 'Domain not found';
+    }
+  }
+
   get domainName(): string {
     return this.$router.currentRoute.params.domainName;
   }
