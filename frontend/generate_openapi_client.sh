87aa7f45e8fa5f1f9aa932557ca46c2d71201f78
Gaganpreet Arora <me@gaganpreet.in>
2021-03-14 15:24:53 +0100

    Update openapi client using snapshot version

--- frontend/generate_openapi_client.sh
+++ frontend/generate_openapi_client.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-openapi-generator generate \
+
+# run.sh script from Github here: https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh
+
+# Using snapshot temporarily until a typescript-axios bug is released: https://github.com/OpenAPITools/openapi-generator/pull/8772
+export OPENAPI_GENERATOR_VERSION=5.1.0-SNAPSHOT
+
+bash $HOME/bin/openapi-generator/run.sh generate \
     -i <(curl "http://localhost/api/v1/openapi.json") \
     -g typescript-axios \
     -o ./src/generated/ \
