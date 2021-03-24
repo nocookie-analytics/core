994d3852660b6eb67f26abaec0ea49e90610fed9
Gaganpreet Arora <me@gaganpreet.in>
2021-03-24 09:49:29 +0100

    Add empty components for domain

---
+++ frontend/src/views/main/domains/EditDomain.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Edit domains</div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+
+export default class EditDomain extends Vue {}
+</script>
---
+++ frontend/src/views/main/domains/ListDomains.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>List domains</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ListDomains extends Vue {}
+</script>
