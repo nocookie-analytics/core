148bfa0081a3632d16ea1db06d315fb8430215f6
Gaganpreet Arora <me@gaganpreet.in>
2021-03-24 12:00:26 +0100

    List domains using API

--- frontend/src/views/main/domains/ListDomains.vue
+++ frontend/src/views/main/domains/ListDomains.vue
@@ -1,10 +1,66 @@
 <template>
-  <div>List domains</div>
+  <div>
+    <v-container fluid>
+      <v-data-table
+        loading="isLoading"
+        :loading-text="loadingText"
+        :headers="headers"
+        :items="domainsList"
+        :items-per-page="50"
+        class="elevation-1"
+        :server-items-length="domainsList.length"
+      ></v-data-table>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
+import { Domain, DomainsApi } from '@/generated';
 import { Component, Vue } from 'vue-property-decorator';
+import { DataTableHeader } from 'vuetify';
 
 @Component
-export default class ListDomains extends Vue {}
+export default class ListDomains extends Vue {
+  fetchDomainsError: string | null = null;
+  domainsList: Array<Domain> = [];
+  isLoading = true;
+
+  async mounted(): Promise<void> {
+    await this.fetchDomains();
+    this.isLoading = false;
+  }
+
+  async fetchDomains(): Promise<void> {
+    const domainsApi = this.$store.getters.domainsApi as DomainsApi;
+    try {
+      const response = await domainsApi.readDomains(0, 50);
+      setTimeout(() => {
+        this.domainsList = response.data;
+      }, 1500);
+    } catch (e) {
+      this.fetchDomainsError =
+        'Could not fetch list of domains, something went wrong. Please try again';
+    }
+  }
+
+  get headers(): Array<DataTableHeader> {
+    return [
+      {
+        text: 'Domain Name',
+        align: 'start',
+        sortable: false,
+        value: 'domain_name',
+      },
+    ];
+  }
+
+  get loadingText(): string {
+    if (this.isLoading) {
+      return 'Loading, please wait';
+    } else if (this.domainsList.length === 0) {
+      return 'Nothing found';
+    }
+    return '';
+  }
+}
 </script>
