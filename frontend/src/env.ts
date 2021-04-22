f170e73ccdae18a84958a92e863f7efbfeac526d
Gaganpreet Arora <me@gaganpreet.in>
2021-04-22 22:27:02 +0200

    use document.domain for figuring out env url instead of env var

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,11 +1,23 @@
-const env = process.env.VUE_APP_ENV;
-
 let envApiUrl = '';
+
+const domain = document.domain;
+
+let env = '';
+
+if (domain === 'test.nocookieanalytics.com') {
+  env = 'test';
+} else if (domain === 'stag.nocookieanalytics.com') {
+  env = 'staging';
+} else if (domain === 'www.nocookieanalytics.com') {
+  env = 'production';
+}
 
 if (env === 'production') {
   envApiUrl = `https://${process.env.VUE_APP_DOMAIN_PROD}`;
 } else if (env === 'staging') {
   envApiUrl = `https://${process.env.VUE_APP_DOMAIN_STAG}`;
+} else if (env == 'test') {
+  envApiUrl = `https://${process.env.VUE_APP_DOMAIN_TEST}`;
 } else {
   envApiUrl = `http://${process.env.VUE_APP_DOMAIN_DEV}`;
 }
