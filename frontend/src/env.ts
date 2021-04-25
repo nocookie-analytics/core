f5313822e5ba8ff1cbf15f7eb4461afab4bfbbf5
Gaganpreet Arora <me@gaganpreet.in>
2021-04-26 00:29:43 +0200

    no need to read env, just use document.domain

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,28 +1,8 @@
-let envApiUrl = '';
-
 const domain = document.domain;
 
-let env = '';
+const protocol = process.env.VUE_APP_ENV === 'development' ? 'http' : 'https';
 
-if (domain === 'test.nocookieanalytics.com') {
-  env = 'test';
-} else if (domain === 'stag.nocookieanalytics.com') {
-  env = 'staging';
-} else if (domain === 'www.nocookieanalytics.com') {
-  env = 'production';
-}
-
-if (env === 'production') {
-  envApiUrl = `https://${process.env.VUE_APP_DOMAIN_PROD}`;
-} else if (env === 'staging') {
-  envApiUrl = `https://${process.env.VUE_APP_DOMAIN_STAG}`;
-} else if (env == 'test') {
-  envApiUrl = `https://${process.env.VUE_APP_DOMAIN_TEST}`;
-} else {
-  envApiUrl = `http://${process.env.VUE_APP_DOMAIN_DEV}`;
-}
-
-console.log(env, envApiUrl);
+const envApiUrl = `${protocol}://${domain}`;
 
 export const apiUrl = envApiUrl;
 export const appName = 'No Cookie Analytics';
