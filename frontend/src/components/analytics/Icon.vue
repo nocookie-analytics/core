7d4249f24a83b69c331099e43d9383d80a1344c3
Gaganpreet Arora <me@gaganpreet.in>
2021-05-12 10:39:15 +0200

    Move icon to separate component

---
+++ frontend/src/components/analytics/Icon.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-icon v-if="iconName">{{ iconName }}</v-icon>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+const iconMap = {
+  Android: 'mdi-android',
+  Linux: 'mdi-linux',
+  'Mac OS X': 'mdi-apple',
+  Windows: 'mdi-microsoft-windows',
+  iOS: 'mdi-apple-ios',
+  Mac: 'mdi-apple',
+
+  Facebook: 'mdi-facebook',
+  Twitter: 'mdi-twitter',
+
+  Chrome: 'mdi-google-chrome',
+  Firefox: 'mdi-firefox',
+};
+
+@Component
+export default class AnalyticsContainer extends Vue {
+  @Prop() public value!: string;
+
+  get iconName(): string | undefined {
+    console.log(iconMap[this.value], this.value);
+    if (iconMap[this.value]) {
+      return iconMap[this.value];
+    }
+    return undefined;
+  }
+}
+</script>
