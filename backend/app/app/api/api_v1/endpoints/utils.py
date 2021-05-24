c2c4ac8da4cb94c13b932e5bf0ad1e5a1969c153
Gaganpreet Arora <me@gaganpreet.in>
2021-05-24 11:11:25 +0200

    Remove celery

--- backend/app/app/api/api_v1/endpoints/utils.py
+++ backend/app/app/api/api_v1/endpoints/utils.py
@@ -6,28 +6,10 @@ from pydantic.networks import EmailStr
 
 from app import models, schemas
 from app.api import deps
-from app.core.celery_app import celery_app
 from app.utils.email import send_test_email
 from app.utils.geolocation import get_ip_from_request
 
 router = APIRouter()
-
-
-@router.post(
-    "/test-celery/",
-    response_model=schemas.Msg,
-    status_code=201,
-    include_in_schema=False,
-)
-def test_celery(
-    msg: schemas.Msg,
-    current_user: models.User = Depends(deps.get_current_active_superuser),
-) -> Any:
-    """
-    Test Celery worker.
-    """
-    celery_app.send_task("app.worker.test_celery", args=[msg.msg])
-    return {"msg": "Word received"}
 
 
 @router.post(
--- backend/app/worker-start.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-set -e
-
-python /app/app/celeryworker_pre_start.py
-
-C_FORCE_ROOT=1 celery -A app.worker worker -l info -Q main-queue -c 1
