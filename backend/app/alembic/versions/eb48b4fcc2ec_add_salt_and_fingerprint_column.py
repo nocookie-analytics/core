e0553ac7cadd197d66c8c00846eecd2b363ec8b5
Gaganpreet Arora <me@gaganpreet.in>
2021-05-24 15:24:51 +0200

    Redo migration

--- backend/app/alembic/versions/eb48b4fcc2ec_add_salt_and_fingerprint_column.py
+++ backend/app/alembic/versions/eb48b4fcc2ec_add_salt_and_fingerprint_column.py
@@ -1,8 +1,8 @@
 """add salt and fingerprint column
 
-Revision ID: 5ac5050aa186
+Revision ID: eb48b4fcc2ec
 Revises: 60d6fb1b4bda
-Create Date: 2021-05-24 12:29:35.773577
+Create Date: 2021-05-24 12:42:58.092429
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5ac5050aa186'
+revision = 'eb48b4fcc2ec'
 down_revision = '60d6fb1b4bda'
 branch_labels = None
 depends_on = None
