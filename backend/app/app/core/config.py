b3b1c3d871f486c8c004c7a44c6115170b5e9243
Gaganpreet Arora <me@gaganpreet.in>
2021-05-27 19:51:38 +0200

    Change email directory

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -67,7 +67,7 @@ class Settings(BaseSettings):
         return v
 
     EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
-    EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/build"
+    EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/compiled"
     EMAILS_ENABLED: bool = False
 
     @validator("EMAILS_ENABLED", pre=True)
