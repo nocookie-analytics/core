1d84723f0d95a4cbc570944a97b6239cbdda23c5
Gaganpreet Arora <me@gaganpreet.in>
2021-06-05 23:32:53 +0200

    Remove trailing space

--- backend/app/scripts/fetch-db-ip.sh
+++ backend/app/scripts/fetch-db-ip.sh
@@ -3,4 +3,4 @@
 curl https://download.db-ip.com/free/dbip-country-lite-2021-06.mmdb.gz -o /tmp/db-ip.mmdb.gz
 gzip -d /tmp/db-ip.mmdb.gz
 mkdir -p /app/backend/app/data/
-mv /tmp/db-ip.mmdb /app/backend/app/data/db-ip-country.mmdb 
+mv /tmp/db-ip.mmdb /app/backend/app/data/db-ip-country.mmdb
