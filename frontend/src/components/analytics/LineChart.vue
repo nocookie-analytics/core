bec5014771583a6959b853d9d04c585d43b9049f
Gaganpreet Arora <me@gaganpreet.in>
2021-06-12 14:15:01 +0200

    Remove unused code

--- frontend/src/components/analytics/LineChart.vue
+++ frontend/src/components/analytics/LineChart.vue
@@ -97,84 +97,20 @@ export default class LineChart extends Vue {
     return data;
   }
 
-  created() {
+  created(): void {
     window.addEventListener('resize', this.resizeChart);
   }
 
-  mounted() {
+  mounted(): void {
     this.width = (this.$refs.wrapper as Element).clientWidth;
   }
 
-  destroyed() {
+  destroyed(): void {
     window.removeEventListener('resize', this.resizeChart);
   }
 
   resizeChart(): void {
     this.width = (this.$refs.wrapper as Element).clientWidth;
   }
-
-  /*
-  get chartData(): ChartData {
-    const labels: Array<string> = [];
-    const totalVisits: Array<number> = [];
-    const visitors: Array<number> = [];
-
-    this.blockData.forEach((row) => {
-      labels.push(row.date);
-      totalVisits.push(row.total_visits);
-      visitors.push(row.visitors);
-    });
-    return {
-      labels,
-      datasets: [
-        {
-          backgroundColor: '#4d698c',
-          label: 'Visitors',
-          fill: true,
-          data: visitors,
-        },
-        {
-          backgroundColor: '#94d2ee',
-          label: 'Total visits',
-          fill: true,
-          data: totalVisits,
-        },
-      ],
-    };
-  }
-  */
-
-  /*
-  get options(): ChartOptions {
-    const options: ChartOptions = {
-      maintainAspectRatio: false,
-      legend: {
-        display: false,
-      },
-      responsive: true,
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-              precision: 0,
-            },
-            gridLines: {
-              display: false,
-            },
-          },
-        ],
-        xAxes: [
-          {
-            gridLines: {
-              display: false,
-            },
-          },
-        ],
-      },
-    };
-    return options;
-  }
-  */
 }
 </script>
