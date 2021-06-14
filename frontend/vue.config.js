43bc497cd7d07bc98cfd448d64f4505176141010
Gaganpreet Arora <me@gaganpreet.in>
2021-06-15 00:05:50 +0200

    Use purgecss

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
   assetsDir: 'static',
@@ -28,7 +30,59 @@ module.exports = {
       .oneOf('normal')
       .use('postcss-loader')
       .tap((options) => {
-        options.plugins.unshift(...[require('cssnano')]);
+        options.plugins.unshift(
+          ...[
+            require('cssnano'),
+            // Shamelessly sourced from
+            // https://github.com/ZeusWPI/g2-frontend/blob/6e6b5beddd17f878b63e44a45b27a919e4679755/postcss.config.js
+            purgecss({
+              content: [
+                `./dist/**/*.html`,
+                `./src/**/*.vue`,
+                `./node_modules/vuetify/src/**/*.ts`,
+                `./node_modules/uplot/dist/**`,
+              ],
+              variables: true,
+              whitelist: [
+                /**
+                 * Vuetify
+                 */
+                'v-application',
+                'v-application--wrap',
+                'layout',
+                'row',
+                'col',
+              ],
+              whitelistPatterns: [
+                /-(leave|enter|appear)(|-(to|from|active))$/,
+                /^(?!(|.*?:)cursor-move).+-move$/,
+                /^router-link(|-exact)-active$/,
+                /data-v-.*/,
+
+                /**
+                 * Vuetify
+                 */
+                /^v-((?!application).)*$/,
+                /^theme--*/,
+                /.*-transition/,
+                /^justify-*/,
+                /^p*-[0-9]/,
+                /^m*-[0-9]/,
+                /^text--*/,
+                /--text$/,
+                /^row-*/,
+                /^col-*/,
+              ],
+              whitelistPatternsChildren: [
+                /**
+                 * Vuetify
+                 */
+                /^v-((?!application).)*$/,
+                /^theme--*/,
+              ],
+            }),
+          ],
+        );
         return options;
       });
 
