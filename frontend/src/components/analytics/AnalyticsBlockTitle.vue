ed0569c88369f2f8ac0b36bbddcfa2f08366a7b6
Gaganpreet Arora <me@gaganpreet.in>
2021-06-20 11:53:42 +0200

    Restore title of blocks

--- frontend/src/components/analytics/AnalyticsBlockTitle.vue
+++ frontend/src/components/analytics/AnalyticsBlockTitle.vue
@@ -1,11 +1,37 @@
-<template><span>Title</span></template>
+<template>
+  <span
+    >{{ block.title }}
+
+    <router-link :to="urlWithRemovedFilter" v-if="isFilterActive">
+      <v-icon> {{ $vuetify.icons.values.delete }} </v-icon>
+    </router-link>
+  </span>
+</template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { DeclarativeBlockType } from './interfaces';
+import { DeclarativeAnalyticsBlock } from './interfaces';
 
 @Component
 export default class AnalyticsBlockTitle extends Vue {
-  @Prop() block!: DeclarativeBlockType;
+  @Prop() block!: DeclarativeAnalyticsBlock;
+
+  get isFilterActive(): boolean {
+    return Boolean(
+      this.block.urlParamName && this.$route.query[this.block.urlParamName],
+    );
+  }
+
+  get urlWithRemovedFilter() {
+    if (this.isFilterActive) {
+      return {
+        query: {
+          ...this.$route.query,
+          [this.block.urlParamName!]: undefined,
+        },
+      };
+    }
+    return undefined;
+  }
 }
 </script>
