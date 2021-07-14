4508d9f7704e3984d8234df78dc7cf44d88507b6
Gaganpreet Arora <me@gaganpreet.in>
2021-07-14 16:47:29 +0200

    Wrap icon in v-btn to avoid focus state on press

--- frontend/src/components/analytics/AnalyticsBlockTitle.vue
+++ frontend/src/components/analytics/AnalyticsBlockTitle.vue
@@ -6,15 +6,18 @@
       <v-icon> {{ $vuetify.icons.values.delete }} </v-icon>
     </router-link>
     <span v-if="totalPages > 1">
-      <v-icon :disabled="currentPage === 0" @click="$emit('prev-page')">{{
-        $vuetify.icons.values.chevronLeft
-      }}</v-icon>
-      {{ currentPage + 1 }}/{{ totalPages }}
-      <v-icon
-        :disabled="currentPage === totalPages - 1"
-        @click="$emit('next-page')"
-        >{{ $vuetify.icons.values.chevronRight }}</v-icon
+      <v-btn icon :disabled="currentPage === 0" @click="$emit('prev-page')"
+        ><v-icon>{{ $vuetify.icons.values.chevronLeft }}</v-icon></v-btn
       >
+      {{ currentPage + 1 }}/{{ totalPages }}
+
+      <v-btn
+        :disabled="currentPage === totalPages - 1"
+        icon
+        @click="$emit('next-page')"
+      >
+        <v-icon>{{ $vuetify.icons.values.chevronRight }}</v-icon>
+      </v-btn>
     </span>
   </span>
 </template>
