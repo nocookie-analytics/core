7a56a0ff782b13108f1bc7b3e5092daef07bdb60
Gaganpreet Arora <me@gaganpreet.in>
2021-10-28 21:26:31 +0200

    Bump dbip version

--- backend/app/scripts/fetch-db-ip.sh
+++ backend/app/scripts/fetch-db-ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eou pipefail
 
-curl -s https://download.db-ip.com/free/dbip-country-lite-2021-08.mmdb.gz -o /tmp/db-ip.mmdb.gz
+curl -s https://download.db-ip.com/free/dbip-country-lite-2021-10.mmdb.gz -o /tmp/db-ip.mmdb.gz
 gzip -d /tmp/db-ip.mmdb.gz
 mv /tmp/db-ip.mmdb backend/app/data/db-ip-country.mmdb
