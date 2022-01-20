b34634e30b180047e79b862c22fc7fae2219296e
Gaganpreet Arora <me@gaganpreet.in>
2022-01-20 22:15:33 +0100

    Make Python implmentation for time_bucket_gapfill more robust

--- backend/app/app/utils/db.py
+++ backend/app/app/utils/db.py
@@ -1,6 +1,6 @@
 import arrow
 from typing import Callable, TypeVar, List, Union
-from datetime import datetime
+from datetime import datetime, timezone
 from app.schemas.analytics import AvgMetricPerDayStat, IntervalType, PageViewsPerDayStat
 
 
@@ -15,8 +15,12 @@ def time_bucket_gapfill(
     creator: Callable,
 ) -> List[T]:
     """A python implementation of the time_bucket_gapfill provided by TimescaleDB."""
-    existing = {row.date: row for row in data}
-    for date in arrow.Arrow.span_range(interval.value, start, end):
-        if date[0] not in existing:
-            existing[date] = creator(date[0].datetime)
+    existing = {row.date.replace(tzinfo=timezone.utc): row for row in data}
+    for date in arrow.Arrow.range(
+        interval.value,
+        arrow.get(start).floor(interval.value),
+        arrow.get(end).ceil(interval.value),
+    ):
+        if date.replace(tzinfo=timezone.utc) not in existing:
+            existing[date] = creator(date.datetime)
     return sorted(existing.values(), key=lambda x: x.date)
