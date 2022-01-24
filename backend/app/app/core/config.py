2a02f28fd4ac367d5d437d2aab4265a5eaa5a403
Gaganpreet Arora <me@gaganpreet.in>
2022-01-24 19:02:47 +0100

    Read SECRET_KEY from env

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -16,7 +16,7 @@ import stripe
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_KEY: str
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SERVER_NAME: Optional[str]
