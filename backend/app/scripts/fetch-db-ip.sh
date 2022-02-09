bcf22efb79e087c570eb76125679d5a162313971
Gaganpreet <me@gaganpreet.in>
2022-02-09 22:42:22 +0100

    Use sentry in frontend (#116)

--- backend/app/scripts/fetch-db-ip.sh
+++ backend/app/scripts/fetch-db-ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eou pipefail
 
-curl -s https://download.db-ip.com/free/dbip-country-lite-2021-12.mmdb.gz -o /tmp/db-ip.mmdb.gz
+curl -s https://download.db-ip.com/free/dbip-country-lite-2022-02.mmdb.gz -o /tmp/db-ip.mmdb.gz
 gzip -d /tmp/db-ip.mmdb.gz
 mv /tmp/db-ip.mmdb backend/app/data/db-ip-country.mmdb
