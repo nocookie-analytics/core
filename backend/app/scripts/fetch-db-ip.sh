f39ed78d90645fdaeca053ad81460d6af8771986
Gaganpreet <me@gaganpreet.in>
2022-11-21 10:58:19 +0100

    Fix broken actions (#219)

--- backend/app/scripts/fetch-db-ip.sh
+++ backend/app/scripts/fetch-db-ip.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eou pipefail
 
-curl -s https://download.db-ip.com/free/dbip-country-lite-2022-07.mmdb.gz -o /tmp/db-ip.mmdb.gz
+# Filename has "year-month" format, parse the latest one first
+version=$(curl -s https://db-ip.com/db/download/ip-to-country-lite | grep free_download_link | grep csv | grep -Eo '20..-..')
+echo "Parsed version ${version}"
+curl -s https://download.db-ip.com/free/dbip-country-lite-"${version}".mmdb.gz -o /tmp/db-ip.mmdb.gz
 gzip -d /tmp/db-ip.mmdb.gz
 mv /tmp/db-ip.mmdb backend/app/data/db-ip-country.mmdb
